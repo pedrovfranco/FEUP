fcab88d90a8ff7b75f28bc6970fdfb4492733278
JoAPSo Pedro Viveiros Franco <up201604828@B02AY0137.fe.up.pt>
2018-02-23 18:00:01 +0000

    Finished class 2 of CAL

--- 2ano/2Semestre/CAL/TP/1/Change.cpp
+++ 2ano/2Semestre/CAL/TP/1/Change.cpp
@@ -1,6 +1,6 @@
 /*
- * Change.cpp
- */
+* Change.cpp
+*/
 
 #include "Change.h"
 #include <string>
@@ -8,47 +8,103 @@
 template<class T>
 void print_array(const T input[], const int &size)
 {
-  for (int i = 0; i < size; i++)
-  {
-    cout << input[i] << ' ';
-  }
-  cout << "\n\n";
+   for (int i = 0; i < size; i++)
+   {
+      cout << input[i] << ' ';
+   }
+   cout << "\n\n";
+}
+
+void print_vector(const vector<int> &input)
+{
+   for (int i = 0; i < input.size(); i++)
+   {
+      cout << input[i] << ' ';
+   }
+   cout << "\n\n";
 }
 
 void generateSave(int &min, string currPath, string &minPath, const int &max, const int *coins, const int &numCoins, int currentSum, int currentNumCoins)
 {
 
-  for (int i = 0; i < numCoins; i++)
-  {
-    if (currentSum + coins[i] == max && currentNumCoins+1 < min)
-    {
-      min = currentNumCoins+1;
-      minPath = currPath+(char)(coins[i]+'0')+";";
-    }
+   for (int i = 0; i < numCoins; i++)
+   {
+      if (currentSum + coins[i] == max && currentNumCoins+1 < min)
+      {
+         min = currentNumCoins+1;
+         minPath = currPath+(char)(coins[i]+'0')+";";
+      }
 
-    if (currentSum + coins[i] <= max)
+      if (currentSum + coins[i] <= max)
       generateSave(min, (currPath + to_string(coins[i]) + ";"), minPath, max, coins, numCoins, currentSum + coins[i], currentNumCoins+1);
-  }
+   }
 
+}
+
+void getTroco(vector<int> &min, vector<int> currPath, const int &max, const int *coins, const int &numCoins, int currentSum)
+{
+   bool vector_incremented = false; // Flag that tells if the size of currPath has incremented or not
+
+   for (int i = 0; i < numCoins; i++)
+   {
+      if (currentSum == max && currPath.size() < min.size()) // If currentpath
+      {
+         min = currPath;
+      }
+
+      if (!vector_incremented) // Made to only increment currPath once
+      {
+         vector_incremented = true;
+         currPath.resize(currPath.size()+1);
+      }
+
+      if (currentSum + coins[i] <= max)
+      {
+         currPath.back() = coins[i];
+         getTroco(min, currPath, max, coins, numCoins, currentSum + coins[i]);
+      }
+   }
+
+}
+
+void generateSave2(vector<vector<int> > &minCombination, vector<int> currPath, const int &max, const int *coins, const int &numCoins, int currentSum)
+{
+   for (int i = 0; i < numCoins; i++)
+   {
+      if ()
+      {
+
+      }
+
+   }
 }
 
 string calcChange(int m, int numCoins, int *coinValues)
 {
-  if (m == 0)
-    return "";
-  
-  if (coinValues[0] > m)
-    return "-";
+   // if (m == 0)
+   // return "";
+   //
+   // if (coinValues[0] > m)
+   // return "-";
+   //
+   // int min = 999;
+   // string minPath;
+   //
+   // generateSave(min, "", minPath, m, coinValues, numCoins, 0, 0);
+   //
+   // for (int i = 0; i < minPath.length()/2; ++i)
+   // {
+   //    swap(minPath[i], minPath[minPath.length() - i - 2]);
+   // }
+   //
+   // return minPath;
 
-  int min = 999;
-  string minPath;
+   vector<int> troco;
+   troco.resize(99);
 
-  generateSave(min, "", minPath, m, coinValues, numCoins, 0, 0);
+   getTroco(troco, {}, m, coinValues, numCoins, 0);
 
-  for (int i = 0; i < minPath.length()/2; ++i)
-  {
-    swap(minPath[i], minPath[minPath.length() - i - 2]);
-  }
+   print_vector(troco);
 
-  return minPath;
+   return "hey";
 }
--- 2ano/2Semestre/CAL/TP/1/Test.cpp
+++ 2ano/2Semestre/CAL/TP/1/Test.cpp
@@ -78,11 +78,11 @@ bool runAllTests(int argc, char const *argv[]) {
 
 int main(int argc, char const *argv[]) {
 
-  // int coins[3] = {1,4,5};
+  int coins[3] = {1,4,5};
 
-  // cout << "\n";
-  // cout << calcChange(stoi(argv[1]), 3, coins);
-  // cout << "\n\n";
+  cout << "\n";
+  cout << calcChange(stoi(argv[1]), 3, coins);
+  cout << "\n\n";
 
-  return runAllTests(argc, argv) ? EXIT_SUCCESS : EXIT_FAILURE;
+  // return runAllTests(argc, argv) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
