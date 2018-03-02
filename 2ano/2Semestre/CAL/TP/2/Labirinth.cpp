98d86098091f9e43338beeea8f41f2d291aad670
JoAPSo Pedro Viveiros Franco <up201604828@B02AY0136.fe.up.pt>
2018-03-02 17:57:55 +0000

    Started exercise 2 of sheet 2 of CAL

---
+++ 2ano/2Semestre/CAL/TP/2/Labirinth.cpp
@@ -0,0 +1,173 @@
+/*
+ * labirinth.cpp
+ */
+
+#include "Labirinth.h"
+
+#include <iostream>
+using namespace std;
+
+
+
+Labirinth::Labirinth(int values[10][10])
+{
+	for (int i = 0; i < 10; i++)
+		for (int j = 0; j < 10; j++)
+			labirinth[i][j] = values[i][j];
+}
+
+
+void Labirinth::initializeVisited()
+{
+	for (int i = 0; i < 10; i++)
+		for (int j = 0; j < 10; j++)
+			visited[i][j] = false;
+}
+
+
+
+
+void  Labirinth::printLabirinth()
+{
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+			cout << labirinth[i][j] << " ";
+
+		cout << endl;
+	}
+}
+
+void Labirinth::findLab(int x, int y, bool &res)
+{
+	if (res)
+		return;
+
+	if (y-1 > -1 && labirinth[y-1][x] == 1) //Up
+	{
+		if (!visited[y-1][x])
+		{
+			visited[y-1][x] = true;
+			findLab(x, y-1, res);
+		}
+	}
+	else if (labirinth[y-1][x] == 2)
+	{
+  		cout << "\nSucess!\n";
+  		res = true;
+  		return;
+	}
+
+
+	if (x+1 < 10 && labirinth[y][x+1] == 1) //Right
+	{
+		if (!visited[y][x+1])
+		{
+			visited[y][x+1] = true;
+			findLab(x+1, y, res);
+		}
+	}
+	else if (labirinth[y][x+1] == 2)
+	{
+		cout << "\nSucess!\n";
+		res = true;
+		return;
+	}
+
+
+	if (y+1 < 10 && labirinth[y+1][x] == 1) //Down
+	{
+		if (!visited[y+1][x])
+		{
+			visited[y+1][x] = true;
+			findLab(x, y+1, res);
+		}
+	}
+	else if (labirinth[y+1][x] == 2)
+	{
+		cout << "\nSucess!\n";
+		res = true;
+		return;
+	}
+
+
+	if (x-1 > -1 && labirinth[y][x-1] == 1) //Left
+	{
+		if (!visited[y][x-1])
+		{
+			visited[y][x-1] = true;
+			findLab(x-1, y, res);
+		}
+	}
+	else if (labirinth[y][x-1] == 2)
+	{
+		cout << "\nSucess!\n";
+		res = true;
+		return;
+	}
+
+
+}
+
+bool Labirinth::findGoal(int x, int y)
+{
+	bool res = false;
+
+	findLab(1,1,res);
+
+	return res;
+}
+
+bool Labirinth::findGoal2(int x, int y)
+{
+	if (y-1 > -1) //Up
+	{
+		if (labirinth[y-1][x] == 1 && !visited[y-1][x])
+		{
+			visited[y-1][x] = true;
+			return findGoal2(x, y-1);
+		}
+		else if (labirinth[y-1][x] == 2)
+	  		return true;
+	}
+
+	if (x+1 < 10) //Right
+	{
+		if (labirinth[y][x+1] == 1 && !visited[y][x+1])
+		{
+			visited[y][x+1] = true;
+			return findGoal2(x+1, y);
+		}
+		else if (labirinth[y][x+1] == 2)
+	  		return true;
+	}
+
+	if (y+1 < 10) //Down
+	{
+		if (labirinth[y+1][x] == 1 && !visited[y+1][x])
+		{
+			visited[y+1][x] = true;
+			return findGoal2(x, y+1);
+		}
+		else if (labirinth[y+1][x] == 2)
+	  		return true;
+	}
+
+	if (x-1 > -1) //Left
+	{
+		if (labirinth[y][x-1] == 1 && !visited[y][x-1])
+		{
+			visited[y][x-1] = true;
+			return findGoal2(x-1, y);
+		}
+		else if (labirinth[y][x-1] == 2)
+	  		return true;
+	}
+
+	// Go back on dead end
+
+
+
+
+	return false;
+}
---
+++ 2ano/2Semestre/CAL/TP/2/Sudoku.cpp
@@ -0,0 +1,135 @@
+/*
+ * Sudoku.cpp
+ *
+ */
+
+#include "Sudoku.h"
+
+/** Inicia um Sudoku vazio.
+ */
+Sudoku::Sudoku()
+{
+	this->initialize();
+}
+
+/**
+ * Inicia um Sudoku com um conte�do inicial.
+ * Lan�a excep��o IllegalArgumentException se os valores
+ * estiverem fora da gama de 1 a 9 ou se existirem n�meros repetidos
+ * por linha, coluna ou bloc 3x3.
+ *
+ * @param nums matriz com os valores iniciais (0 significa por preencher)
+ */
+Sudoku::Sudoku(int nums[9][9])
+{
+	this->initialize();
+
+	for (int i = 0; i < 9; i++)
+	{
+		for (int j = 0; j < 9; j++)
+		{
+			if (nums[i][j] != 0)
+			{
+				int n = nums[i][j];
+				numbers[i][j] = n;
+				lineHasNumber[i][n] = true;
+				columnHasNumber[j][n] = true;
+				block3x3HasNumber[i / 3][j / 3][n] = true;
+				countFilled++;
+			}
+		}
+	}
+}
+
+void Sudoku::initialize()
+{
+	for (int i = 0; i < 9; i++)
+	{
+		for (int j = 0; j < 9; j++)
+		{
+			for (int n = 0; n < 10; n++)
+			{
+				numbers[i][j] = 0;
+				lineHasNumber[i][n] = false;
+				columnHasNumber[j][n] = false;
+				block3x3HasNumber[i / 3][j / 3][n] = false;
+			}
+		}
+	}
+
+	this->countFilled = 0;
+}
+
+/**
+ * Obtem o conte�do actual (s� para leitura!).
+ */
+int** Sudoku::getNumbers()
+{
+	int** ret = new int*[9];
+
+	for (int i = 0; i < 9; i++)
+	{
+		ret[i] = new int[9];
+
+		for (int a = 0; a < 9; a++)
+			ret[i][a] = numbers[i][a];
+	}
+
+	return ret;
+}
+
+/**
+ * Verifica se o Sudoku j� est� completamente resolvido
+ */
+bool Sudoku::isComplete()
+{
+	return countFilled == 9 * 9;
+}
+
+void Sudoku::solve2(int i, int j, bool &ret)
+{
+	if (ret)
+		return;
+
+		if (numbers[i][j] == 0)
+		{
+			if ()
+		}
+
+}
+
+/**
+ * Resolve o Sudoku.
+ * Retorna indica��o de sucesso ou insucesso (sudoku imposs�vel).
+ */
+bool Sudoku::solve()
+{
+	bool ret = false,
+
+	for (int i = 0; i < 9; i++)
+	{
+		for (int j = 0; j < 9; j++)
+		{
+			solve2(i, j, ret);
+		}
+	}
+
+
+	return ret;
+}
+
+
+
+/**
+ * Imprime o Sudoku.
+ */
+void Sudoku::print()
+{
+	for (int i = 0; i < 9; i++)
+	{
+		for (int a = 0; a < 9; a++)
+			cout << this->numbers[i][a] << " ";
+
+		cout << endl;
+	}
+}
