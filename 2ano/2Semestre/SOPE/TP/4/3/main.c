e44735b21edceb856fbe8f8268371567e744e417
pedrofixe <yurbypedro@hotmai.com>
2018-03-08 15:43:22 +0000

    Still on exercise 3 of SOPE sheet #4

--- 2ano/2Semestre/SOPE/TP/4/3/main.c
+++ 2ano/2Semestre/SOPE/TP/4/3/main.c
@@ -3,87 +3,142 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
-void usr1_handler(int signo)
+static int increasing = 1; // 1 == increasing, 0 == decreasing
+static int value = 0;
+
+void sigusr_handler(int signo)
 {
- printf("Entering SIGINT handler ...\n");
- sleep(5);
- printf("Exiting SIGINT handler ...\n");
+	if (signo == SIGUSR1)
+			increasing = 1;
+	else if (signo == SIGUSR2)
+		increasing = 0;
+	else
+		printf("Handler error!\n");
 }
 
-void usr2_handler(int signo)
-{
- printf("Entering SIGTERM handler ...\n");
- printf("Exiting SIGTERM handler ...\n");
-}
 
 int a(int argc, char const *argv[])
 {
-	struct sigaction usr1action;
+	struct sigaction usraction;
 
-	usr1action.sa_handler = sigint_handler;
-	sigemptyset(&action.sa_mask);
-	usr1action.sa_flags = 0;
+	usraction.sa_handler = sigusr_handler;
+	sigemptyset(&usraction.sa_mask);
+	usraction.sa_flags = 0;
 
-	if (sigaction(SIGINT,&action,NULL) < 0)
+	if (sigaction(SIGUSR1, &usraction, NULL) != 0)
 	{
 		fprintf(stderr,"Unable to install SIGINT handler\n");
 		return 1;
 	}
 
-	printf("Try me with CTRL-C ...\n");
+	if (sigaction(SIGUSR2, &usraction, NULL) != 0)
+	{
+		fprintf(stderr,"Unable to install SIGINT handler\n");
+		return 1;
+	}
+
+
 	while(1)
 	{
-		pause();
+		if (increasing)
+			value++;
+		else
+			value--;
+
+		printf("\n%i", value);
+
+		sleep(1);
 	}
+
 
 	return 0;
 }
 
-int c(int argc, char const *argv[])
+
+int b(int argc, char const *argv[])
 {
-	struct sigaction sigintaction;
+	struct sigaction usraction;
 
-	sigintaction.sa_handler = sigint_handler;
-	sigemptyset(&sigintaction.sa_mask);
-	sigintaction.sa_flags = 0;
+	usraction.sa_handler = sigusr_handler;
+	sigemptyset(&usraction.sa_mask);
+	usraction.sa_flags = 0;
 
-	if (sigaction(SIGINT,&sigintaction,NULL) < 0)
+	if (sigaction(SIGUSR1, &usraction, NULL) != 0)
 	{
 		fprintf(stderr,"Unable to install SIGINT handler\n");
 		return 1;
 	}
 
-	struct sigaction sigtermaction;
-
-	sigtermaction.sa_handler = sigterm_handler;
-	sigemptyset(&sigtermaction.sa_mask);
-	sigtermaction.sa_flags = 0;
-
-	if (sigaction(SIGTERM,&sigtermaction,NULL) < 0)
+	if (sigaction(SIGUSR2, &usraction, NULL) != 0)
 	{
-		fprintf(stderr,"Unable to install SIGTERM handler\n");
+		fprintf(stderr,"Unable to install SIGINT handler\n");
 		return 1;
 	}
 
-	printf("Try me with CTRL-C ...\n");
-	while(1)
+	pid_t pid;
+	pid = fork();
+
+
+	unsigned int printCounter = 0;
+	while (printCounter < 50)
 	{
-		pause();
+
+		if (increasing)
+		{
+			printCounter++;
+			value++;
+		}
+		else
+		{
+			printCounter++;
+			value--;
+		}
+
+		if (pid > 0) // Parent
+		{
+			if (increasing)
+				kill(pid, SIGUSR1);
+			else
+				kill(pid, SIGUSR2);
+
+			if (printCounter >= 50)
+				kill(pid, SIGTERM);
+		}
+
+		else if (pid == 0)// Child
+		{
+			printf("\n%i", value); 
+			printCounter++;
+		}
+
+		else // Error
+		{
+			return 1;
+		}
+
+		sleep(1);
+
 	}
+
+	// if (pid > 0) //Parent
+	// {
+	// 	kill(pid, SIGTERM);
+	// }
 
 	return 0;
 }
-
 
 int main(int argc, char const *argv[])
 {
 	if (argv[1] == 0 || strcmp(argv[1], "a") == 0)
 		return a(argc, argv);
-	// else if (strcmp(argv[1], "b") == 0)
-	// 	return b(argc, argv);
-	else if (strcmp(argv[1], "c") == 0)
-		return c(argc, argv);
+	else if (strcmp(argv[1], "b") == 0)
+		return b(argc, argv);
+	// else if (strcmp(argv[1], "c") == 0)
+	// 	return c(argc, argv);
 
 	return 0;
 }
